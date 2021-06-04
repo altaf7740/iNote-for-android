9ea36e916c3511f5e1ee8b226bd2c7a6c476b3b1
Mohammad Altaf Hussain <iamaltaf@protonmail.com>
2021-06-04 11:17:32 +0530

    fresh

---
+++ app/src/main/java/com/official/myapplication/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.official.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import static com.official.myapplication.R.layout.activity_splash;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activity_splash);
+    }
+}
